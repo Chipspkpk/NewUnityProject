45c35c9e2ee09dc1e8aa2e039ebf468bc4184b8f
Chipspkpk <Nothing>
2018-04-30 21:03:22 +0900

    GameJam:ブロック崩し->PopCorn

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,70 @@
+﻿//using UnityEngine;
+//using System.Collections;
+
+//public class BlockController : MonoBehaviour {
+
+//	//// Use this for initialization
+//	void Start () {
+////        StartCoroutine(Move());
+//    }
+	
+//	//// Update is called once per frame
+//	//void Update () {
+	
+//	//}
+
+
+//    public float accel = 1000;
+
+//    private void Update()
+//    {
+//        var force = transform.right * Input.GetAxisRaw("Horizontal") * accel;
+//        GetComponent<Rigidbody2D>().AddForce(force);
+//    }
+
+//    //IEnumerator Move()
+//    //{
+//    //    while (true)
+//    //    {
+//    //        var force = transform.right * Input.GetAxisRaw("Horizontal") * accel;
+//    //        GetComponent<Rigidbody2D>().AddForce(force);
+//    //        yield return new WaitForSeconds(0.0f);
+//    //    }
+//    //}
+
+
+//}
+
+
+using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour
+{
+    private Vector3 pos;
+    private Vector3 WorldPointPos;
+
+    void Update()
+    {
+        //マウス位置座標をVector3で取得
+        pos = Input.mousePosition;
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        WorldPointPos = Camera.main.ScreenToWorldPoint(pos);
+
+        // 壁を突き抜けないようにx軸の移動範囲を限定
+        if (WorldPointPos.x <= -3.5f)
+        {
+            WorldPointPos.x = -3.5f;
+        }
+        else if (WorldPointPos.x >= 3.5f)
+        {
+            WorldPointPos.x = 3.5f;
+        }
+        //y軸とz軸は固定
+        WorldPointPos.y = 4.0f;
+        WorldPointPos.z = 0.0f;
+
+        // ワールド座標をPlayer位置へ変換
+        gameObject.transform.position = WorldPointPos;
+    }
+}
---
+++ Assets/Scripts/BlockMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMovement : MonoBehaviour {
+    public Texture2D tex;
+    public float timeOut = 1.0f;
+    private float timeElapsed;
+
+    private Rigidbody2D rb2D;
+    private Sprite mySprite;
+    private SpriteRenderer sr;
+    public float thrust = 100.0f;
+
+    //void Awake()
+    //{
+    //    sr = gameObject.AddComponent<SpriteRenderer>();
+    //    rb2D = gameObject.AddComponent<Rigidbody2D>();
+    //}
+
+    void Start()
+    {
+        rb2D =GetComponent<Rigidbody2D>();
+        thrust = Random.Range(0f, 15.0f);
+        //        mySprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, 128.0f, 128.0f), new Vector2(0.5f, 0.5f), 100.0f);
+        //timeOut = 10.0f;
+        //        sr.color = new Color(0.9f, 0.9f, 0.5f, 1.0f);
+        //        sr.sprite = mySprite;
+        //  rb = gameObject.AddComponent<Rigidbody>();
+        //transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        StartCoroutine(RandomMove());
+    }
+
+//    void Update()
+//    {
+//        timeElapsed += Time.deltaTime;
+    //    rb2D.AddForce(new Vector2(20, 1));
+//        if(timeElapsed >= timeOut)
+//        if(Input.GetMouseButtonDown(0))
+//        {
+            //Vector2 dir = new Vector2(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            //dir.Normalize();
+            //rb2D.AddForce(dir * 3.0f);
+            //timeElapsed = 0.0f;
+//        }
+//    }
+
+    IEnumerator RandomMove()
+    {
+        while (true)
+        {
+            Vector2 dir = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+            dir.Normalize();
+            rb2D.AddForce(dir * 100.0f);
+            timeElapsed = 0.0f;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    //private IEnumerator Movement()
+    //{
+
+    //}
+}
